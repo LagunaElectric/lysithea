dbf19ad56d8c15d54a4119b65ea87766212d23be
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-12-27 13:03:45 -0500

    Added boilerplate UI to start solving transpilation

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,21 @@
 import ReactDOM from 'react-dom'
+import { useState } from 'react'
 
 const App = () => {
-  return <div>Hello World</div>
+  const [input, setInput] = useState('')
+  const [code, setCode] = useState('')
+
+  const onClick = () => {
+    setCode(input)
+  }
+
+  return <div>
+    <textarea value={ input } onChange={ e => setInput(e.currentTarget.value) }></textarea>
+    <div>
+      <button onClick={ onClick }>submit</button>
+    </div>
+    <pre>{ code }</pre>
+  </div>
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
