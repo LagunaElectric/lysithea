f3d92245b1dd9e0d8613b42e0150e296bf635595
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-12-27 21:07:23 -0500

    Modified onResolve to use a URL

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -6,7 +6,14 @@ export const unpkgPathPlugin = () => {
     setup(build: esbuild.PluginBuild) {
       build.onResolve({ filter: /.*/ }, async (args: any) => {
         console.log("onResolve", args)
-        return { path: args.path, namespace: "a" }
+        if (args.path === "index.js") {
+          return { path: args.path, namespace: "a" }
+        } else if (args.path === "tiny-test-pkg") {
+          return {
+            namespace: "a",
+            path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js"
+          }
+        }
       })
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
