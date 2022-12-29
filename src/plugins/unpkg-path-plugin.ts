f3b7ef877bde2fbd098217b261b8afeed71c3a79
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-12-28 20:46:09 -0500

    Importing now works on modules with nesting file structure

--- src/plugins/unpkg-path-plugin.ts
+++ src/plugins/unpkg-path-plugin.ts
@@ -14,7 +14,8 @@ export const unpkgPathPlugin = () => {
         if (args.path.includes("./") || args.path.includes("../")) {
           return {
             namespace: "a",
-            path: new URL(args.path, `${args.importer}/`).href
+            path: new URL(args.path, `https://unpkg.com${args.resolveDir}/`)
+              .href
           }
         }
 
@@ -22,13 +23,6 @@ export const unpkgPathPlugin = () => {
           namespace: "a",
           path: `https://unpkg.com/${args.path}`
         }
-
-        // else if (args.path === "tiny-test-pkg") {
-        //   return {
-        //     namespace: "a",
-        //     path: "https://unpkg.com/tiny-test-pkg@1.0.0/index.js"
-        //   }
-        // }
       })
 
       build.onLoad({ filter: /.*/ }, async (args: any) => {
@@ -38,16 +32,17 @@ export const unpkgPathPlugin = () => {
           return {
             loader: "jsx",
             contents: `
-              const message = require('medium-test-pkg');
+              const message = require('nested-test-pkg');
               console.log(message);
             `
           }
         }
 
-        const { data } = await axios.get(args.path)
+        const { data, request } = await axios.get(args.path)
         return {
           loader: "jsx",
-          contents: data
+          contents: data,
+          resolveDir: new URL("./", request.responseURL).pathname
         }
       })
     }
