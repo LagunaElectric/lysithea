465c19720063fe8e802b5ccba281d42a9a336c16
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2022-12-28 23:36:42 -0500

    Refactor: Broke the onLoad logic out from unpkg-path-plugin to a new fetch-plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import ReactDOM from 'react-dom'
-import { useEffect, useState, useRef } from 'react'
 import * as esbuild from 'esbuild-wasm'
+import { useEffect, useState, useRef } from 'react'
+import { fetchPlugin } from './plugins/fetch-plugin'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
@@ -27,14 +28,15 @@ const App = () => {
       entryPoints: ['index.js'],
       bundle: true,
       write: false,
-      plugins: [unpkgPathPlugin(input)],
+      plugins: [
+        unpkgPathPlugin(),
+        fetchPlugin(input)
+      ],
       define: {
         'process.env.NODE_ENV': '"production"',
         global: 'window'
       }
     })
-
-    // console.log(result)
 
     setCode(result.outputFiles[0].text)
   }
