eebf5e14a00bdcabeeee193c7dd43eb835ce0195
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-01 00:46:57 -0500

    refactor: take bundling logic out of the main app file

--- src/index.tsx
+++ src/index.tsx
@@ -1,46 +1,16 @@
-import * as esbuild from 'esbuild-wasm'
+import Bundler from './bundler'
 import Preview from './components/preview'
 import { createRoot } from 'react-dom/client'
 import CodeEditor from './components/code-editor'
-import { useEffect, useState, useRef } from 'react'
-import { fetchPlugin } from './plugins/fetch-plugin'
-import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
+import { useState } from 'react'
 
 const App = () => {
-  const esBuildRef = useRef<any>()
   const [input, setInput] = useState('')
   const [code, setCode] = useState('')
 
-  const startService = async () => {
-    esBuildRef.current = await esbuild.startService({
-      worker: true,
-      wasmURL: 'https://www.unpkg.com/esbuild-wasm@0.8.27/esbuild.wasm'
-    })
-  }
-
-  useEffect(() => {
-    startService()
-  }, [])
-
-
   const onClick = async () => {
-    if (!esBuildRef.current) return
-
-    const result = await esBuildRef.current.build({
-      entryPoints: ['index.js'],
-      bundle: true,
-      write: false,
-      plugins: [
-        unpkgPathPlugin(),
-        fetchPlugin(input)
-      ],
-      define: {
-        'process.env.NODE_ENV': '"production"',
-        global: 'window'
-      }
-    })
-
-    setCode(result.outputFiles[0].text)
+    const output = await Bundler(input)
+    setCode(output)
   }
 
 
