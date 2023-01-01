353baa513b6fb1a69c6937d9f476bbc9a94981cf
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-01 11:14:29 -0500

    feat: Add resizable box to our code cells

--- src/components/code-cell.tsx
+++ src/components/code-cell.tsx
@@ -2,6 +2,7 @@ import Bundler from '../bundler'
 import Preview from './preview'
 import CodeEditor from './code-editor'
 import { useState } from 'react'
+import Resizable from './resizable'
 
 const CodeCell = () => {
   const [input, setInput] = useState('')
@@ -14,14 +15,16 @@ const CodeCell = () => {
 
 
   return <div>
-    <CodeEditor
-      initialValue='// code'
-      onChange={ value => setInput(value) }
-    />
-    <div>
-      <button onClick={ onClick }>submit</button>
-    </div>
-    <Preview code={ code } />
+    <Resizable direction='vertical'>
+      <CodeEditor
+        initialValue='// code'
+        onChange={ value => setInput(value) }
+      />
+      <div>
+        <button onClick={ onClick }>submit</button>
+      </div>
+      <Preview code={ code } />
+    </Resizable>
   </div>
 }
 
---
+++ src/components/resizable.tsx
@@ -0,0 +1,20 @@
+import { ResizableBox } from "react-resizable"
+
+interface ResizableProps {
+  direction: 'horizontal' | 'vertical',
+  children?: React.ReactNode
+}
+
+const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  return (
+    <ResizableBox
+      width={ 300 }
+      height={ 300 }
+      resizeHandles={ ['s'] }
+    >
+      { children }
+    </ResizableBox>
+  )
+}
+
+export default Resizable
