235cc41b4fc70285b407cbb9dcd5ac3251177cbb
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-01 12:42:21 -0500

    add directional logic to the resizable component

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,4 @@
-import { ResizableBox } from "react-resizable"
+import { ResizableBox, ResizableBoxProps } from "react-resizable"
 import './resizable.css'
 
 interface ResizableProps {
@@ -7,13 +7,28 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+  let resizableProps: ResizableBoxProps
+
+  if (direction === 'horizontal') {
+    resizableProps = {
+      minConstraints: [window.innerWidth * 0.2, Infinity],
+      maxConstraints: [window.innerWidth * 0.75, Infinity],
+      height: Infinity,
+      width: window.innerWidth * 0.75,
+      resizeHandles: ['e'],
+    }
+  } else {
+    resizableProps = {
+      minConstraints: [Infinity, 24],
+      maxConstraints: [Infinity, window.innerHeight * 0.9],
+      height: 300,
+      width: Infinity,
+      resizeHandles: ['s'],
+    }
+  }
+
   return (
-    <ResizableBox
-      width={ Infinity }
-      height={ 300 }
-      resizeHandles={ ['s'] }
-      maxConstraints={ [Infinity, window.innerHeight * 0.9] }
-    >
+    <ResizableBox { ...resizableProps }>
       { children }
     </ResizableBox>
   )
