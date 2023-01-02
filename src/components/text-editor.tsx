a6c2cb6f4e7ccbfb29d6420a99fde0eaff2c1103
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-01 22:29:45 -0500

    feat: implement toggling between editor and preview mode for markdown editor

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,9 +1,42 @@
 import MDEditor from "@uiw/react-md-editor"
+import { useState, useEffect, useRef } from "react"
 
 const TextEditor: React.FC = () => {
+  const [editing, setEditing] = useState(false)
+  const editorRef = useRef<HTMLDivElement | null>(null)
+
+  useEffect(() => {
+    const listener = (event: MouseEvent) => {
+      if (
+        event.target &&
+        event.target instanceof HTMLElement &&
+        editorRef.current &&
+        editorRef.current.contains(event.target)
+      ) {
+        return
+      }
+
+      setEditing(false)
+    }
+
+    document.addEventListener('click', listener, { capture: true })
+
+    return () => {
+      document.removeEventListener('click', listener, { capture: true })
+    }
+  }, [])
+
+  if (editing) {
+    return (
+      <div ref={ editorRef }>
+        <MDEditor />
+      </div>
+    )
+  }
+
   return (
-    <div>
-      <MDEditor />
+    <div onClick={ () => setEditing(true) }>
+      <MDEditor.Markdown source="# this is the best" />
     </div>
   )
 }
