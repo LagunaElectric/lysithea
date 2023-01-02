a815fa8cefa8e92c0ee912d9b8820615d0e13b5f
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-01 22:15:13 -0500

    feat: begin text editor implementation

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,11 @@
 import { createRoot } from 'react-dom/client'
-import CodeCell from './components/code-cell'
+import TextEditor from './components/text-editor'
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 
 const App = () => {
   return (
     <div>
-      <CodeCell />
+      <TextEditor />
     </div>
   )
 }
