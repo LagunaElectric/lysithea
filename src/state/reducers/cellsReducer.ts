c2872ec8cd051c474cb43687e11e4dd1496b88d9
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-05 02:16:06 -0500

    simplify update action with immer and implement delete cell action

--- src/state/reducers/cellsReducer.ts
+++ src/state/reducers/cellsReducer.ts
@@ -1,3 +1,4 @@
+import produce from "immer"
 import { ActionType } from "../action-types"
 import { Action } from "../actions"
 import Cell from "../cell"
@@ -18,25 +19,16 @@ const initialState: CellsState = {
   data: {}
 }
 
-const reducer = (
-  state: CellsState = initialState,
-  action: Action
-): CellsState => {
+const reducer = produce((state: CellsState = initialState, action: Action) => {
   switch (action.type) {
     case ActionType.UPDATE_CELL:
       const { id, content } = action.payload
-      return {
-        ...state,
-        data: {
-          ...state.data,
-          [id]: {
-            ...state.data[id],
-            content
-          }
-        }
-      }
+      state.data[id].content = content
+      break
     case ActionType.DELETE_CELL:
-      return state
+      delete state.data[action.payload]
+      state.order = state.order.filter((id) => id !== action.payload)
+      break
     case ActionType.MOVE_CELL:
       return state
     case ActionType.INSERT_CELL:
@@ -44,6 +36,6 @@ const reducer = (
     default:
       return state
   }
-}
+})
 
 export default reducer
