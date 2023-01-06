da99dadfb73e633afe646decd81e0cff92598026
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-06 00:51:09 -0500

    implement cell lists and their items

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,5 +1,15 @@
-const CellListItem: React.FC = () => {
-  return <div>Cell List Item</div>
+import { Cell } from "../state"
+import CodeCell from "./code-cell"
+import TextEditor from "./text-editor"
+
+interface CellListItemProps {
+  cell: Cell
+}
+
+const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
+  let child: JSX.Element = cell.type === 'code' ? <CodeCell /> : <TextEditor />
+
+  return <div>{ child }</div>
 }
 
 export default CellListItem
--- src/state/cell.ts
+++ src/state/cell.ts
@@ -1,6 +1,6 @@
 export type CellTypes = "text" | "code"
 
-export default interface Cell {
+export interface Cell {
   id: string
   type: CellTypes
   content: string
