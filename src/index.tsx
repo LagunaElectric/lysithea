8a2305c0a8e924072acd21712ec33f00e3f84821
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-06 00:28:05 -0500

    begin working on cell list and its items

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
-import TextEditor from './components/text-editor'
 import { Provider } from 'react-redux'
 import { store } from './state'
+import CellList from './components/cell-list'
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
 
 const App = () => {
   return (
     <Provider store={ store }>
       <div>
-        <TextEditor />
+        <CellList />
       </div>
     </Provider>
   )
