65d1665d3c5f32aed7c2c2dc712f21cc3836a0be
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-06 02:16:07 -0500

    feat: add buttons to move and delete cells

--- src/components/cell-list-item.tsx
+++ src/components/cell-list-item.tsx
@@ -1,4 +1,5 @@
 import { Cell } from "../state"
+import ActionBar from "./action-bar"
 import CodeCell from "./code-cell"
 import TextEditor from "./text-editor"
 
@@ -9,7 +10,12 @@ interface CellListItemProps {
 const CellListItem: React.FC<CellListItemProps> = ({ cell }) => {
   let child: JSX.Element = cell.type === 'code' ? <CodeCell cell={ cell } /> : <TextEditor cell={ cell } />
 
-  return <div>{ child }</div>
+  return (
+    <div>
+      <ActionBar id={ cell.id } />
+      { child }
+    </div>
+  )
 }
 
 export default CellListItem
