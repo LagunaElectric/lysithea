50bffa25bd2ba1b7b5b457546f0efd07cee1af2a
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-06 23:40:35 -0500

    add additional styling and polish to AddCell

--- src/components/add-cell.tsx
+++ src/components/add-cell.tsx
@@ -4,16 +4,28 @@ import { useActions } from '../hooks/use-actions'
 interface AddCellProps {
   nextCellId: string | null
   forceVisible?: boolean
-  isBottom?: boolean
 }
 
-const AddCell: React.FC<AddCellProps> = ({ nextCellId, forceVisible, isBottom }) => {
+const AddCell: React.FC<AddCellProps> = ({ nextCellId, forceVisible }) => {
   const { insertCell } = useActions()
 
   return (
-    <div>
-      <button onClick={ () => insertCell(nextCellId, "code", "up") }>Code</button>
-      <button onClick={ () => insertCell(nextCellId, "text", "up") }>Text</button>
+    <div className='add-cell' >
+      <div className="add-buttons">
+        <button className="button is-rounded is-primary is-small" onClick={ () => insertCell(nextCellId, "code", "up") }>
+          <span className="icon is-small">
+            <i className="fas fa-plus" />
+          </span>
+          <span>Code</span>
+        </button>
+        <button className="button is-rounded is-primary is-small" onClick={ () => insertCell(nextCellId, "text", "up") }>
+          <span className="icon is-small">
+            <i className="fas fa-plus" />
+          </span>
+          <span>Text</span>
+        </button>
+      </div>
+      <div className="divider" />
     </div>
   )
 }
