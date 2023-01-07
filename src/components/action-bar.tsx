15831298f6a8731f2e5ff620bf3dcf501ae88c57
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-06 20:45:53 -0500

    add icons and styling to the action buttons

--- src/components/action-bar.tsx
+++ src/components/action-bar.tsx
@@ -6,11 +6,25 @@ interface ActionBarProps {
 
 const ActionBar: React.FC<ActionBarProps> = ({ id }) => {
   const { moveCell, deleteCell } = useActions()
+
+  // TODO: refactor buttons to their own component
   return (
     <div>
-      <button onClick={ () => moveCell(id, 'up') }>Up</button>
-      <button onClick={ () => moveCell(id, 'down') } >Down</button>
-      <button onClick={ () => deleteCell(id) } >Delete</button>
+      <button className="button is-primary is-small" onClick={ () => moveCell(id, 'up') }>
+        <span className="icon">
+          <i className="fas fa-arrow-up" />
+        </span>
+      </button>
+      <button className="button is-primary is-small" onClick={ () => moveCell(id, 'down') } >
+        <span className="icon">
+          <i className="fas fa-arrow-down" />
+        </span>
+      </button>
+      <button className="button is-primary is-small" onClick={ () => deleteCell(id) } >
+        <span className="icon">
+          <i className="fas fa-trash" />
+        </span>
+      </button>
     </div>
   )
 }
