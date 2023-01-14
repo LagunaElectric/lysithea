ed9b668a3df9800188e1d1aa07cac23ee3aaa1d9
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-14 13:30:09 -0500

    fix: prevent cells from snapping when dragging near the bottom of the screen

--- src/components/cell-list.tsx
+++ src/components/cell-list.tsx
@@ -2,6 +2,7 @@ import { useTypedSelector } from "../hooks/use-typed-selector"
 import CellListItem from "./cell-list-item"
 import AddCell from "./add-cell"
 import { Fragment } from "react"
+import './cell-list.css'
 
 const CellList: React.FC = () => {
   const cells = useTypedSelector(({ cells: { order, data } }) => order.map((id) => data[id]))
@@ -13,7 +14,7 @@ const CellList: React.FC = () => {
     </Fragment>
   ))
 
-  return <div>
+  return <div className="cell-list">
     <AddCell prevCellId={ null } />
     { renderedCells }
   </div>
