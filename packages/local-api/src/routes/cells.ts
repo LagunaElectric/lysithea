c086ad12dbe51fc6760085f4ca4e45d69ef22138
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-16 00:41:04 -0500

    implement post method for cells

--- packages/local-api/src/routes/cells.ts
+++ packages/local-api/src/routes/cells.ts
@@ -1,18 +1,26 @@
 import express from "express"
+import fs from "fs/promises"
+import path from "path"
+
+interface Cell {
+  id: string
+  content: string
+  type: "text" | "code"
+}
 
 export const createCellsRouter = (filename: string, dir: string) => {
   const router = express.Router()
 
-  router.get("/cells", (req, res) => {
+  router.get("/cells", async (req, res) => {
     res.send("List of Cells")
   })
 
-  router.post("/cells", (req, res) => {
-    const {
-      body: { content, type }
-    } = req
+  router.post("/cells", async (req, res) => {
+    const { cells }: { cells: Cell[] } = req.body
 
-    res.send(`Posting to Cells: ${content} ${type}`)
+    await fs.writeFile(path.join(dir, filename), JSON.stringify(cells), "utf-8")
+
+    res.send({ status: "ok" })
   })
 
   return router
