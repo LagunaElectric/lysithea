9e2a3c7834fe5c832b05b4cf9c8c2d093355f7d8
Ronald Jimmy Headrick III <6085143+LagunaElectric@users.noreply.github.com>
2023-01-17 19:01:58 -0500

    chore: remove temporary actions used to populate the app for development

--- packages/local-client/src/state/store.ts
+++ packages/local-client/src/state/store.ts
@@ -4,21 +4,3 @@ import reducers from "./reducers"
 import { ActionType } from "./action-types"
 
 export const store = createStore(reducers, {}, applyMiddleware(thunk))
-
-store.dispatch({
-  type: ActionType.INSERT_CELL,
-  payload: {
-    id: "w45gw4",
-    type: "code",
-    direction: "up"
-  }
-})
-
-store.dispatch({
-  type: ActionType.INSERT_CELL,
-  payload: {
-    id: "w45ghw",
-    type: "text",
-    direction: "up"
-  }
-})
